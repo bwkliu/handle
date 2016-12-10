45a44dcd926398fb90f9eb3fa4e21bb0879ec031
bwkliu <bwkliu@qq.com>
2016-12-10 12:29:46 +0800

    st

--- HandleContract.py
+++ HandleContract.py
@@ -12,7 +12,8 @@ import sys,os.path, time,datetime,pytz
 from collections import OrderedDict
 import pandas as pd
 from Util import Util
-
+import redis
+from multiprocessing import Queue
 
 
 
@@ -23,15 +24,23 @@ class HandleHisData:
   def __init__(self,conn):
     self.m_conn=conn
     self.tick_contract_Map={}
+    self.taskQueue=Queue()
+    
+    self.pool = redis.ConnectionPool(host='127.0.0.1', port=6379)  
+    self.redis = redis.Redis(connection_pool=self.pool)
     
     self.write2file = False
     self.newRowData = []
     self.finished = False # true when historical data is done
     
+    
+    
     self.m_endESTDateStr = ''
     self.m_endESTtimeStr = ''
     self.m_barLength = ''
     self.m_duration=''
+    
+    #self.logFile=open('./logfile.txt', 'w+')
   
   def setContract(self,tickId,contract,endDateStr,duration='2 D',barLength='1 day',whatToShow='TRADES',useRTH=0,formatDate=1):
     #endDateStr= dateStr + ' 23:59:59 EST'
@@ -48,7 +57,7 @@ class HandleHisData:
     self.tick_contract_Map[tickId]['formatDate']=formatDate
   
   def reviceHisData(self,msg,msgTZ='US/Eastern'):
-    #print msg
+    #print >> self.logFile, msg
     if int(msg.high) > 0:
       estTZ=pytz.timezone('US/Eastern')
       estDatetime=''
@@ -59,14 +68,15 @@ class HandleHisData:
       estDatetime=estTZ.localize(estDatetime)
       NYtimeStr=estDatetime.strftime('%Y%m%d %H:%M:%S')
       dataStr =  '%s,%s,%s,%s,%s,%s' % (NYtimeStr,msg.open,msg.high,msg.low,msg.close,msg.volume)      
-      self.newRowData.append(dataStr+'\n')
-    else:
-      self.finished = True
-      self.write2file = True
-      if len(self.newRowData)  > 0 and self.write2file:
-        if self.tick_contract_Map.has_key(msg.reqId):
-          reqDic = self.tick_contract_Map[msg.reqId]
-          self.appendToFile('HistData',reqDic['contract'].m_symbol,reqDic['contract'].m_secType,reqDic['endDateStr'].split(' ')[0],reqDic['endDateStr'],reqDic['barLength'])
+      if self.tick_contract_Map.has_key(msg.reqId):        
+        self.tick_contract_Map[msg.reqId]['reviceData'].append(dataStr+'\n')   
+    elif int(msg.high) == -1:
+      if self.tick_contract_Map.has_key(msg.reqId):
+        reqDic = self.tick_contract_Map[msg.reqId]
+        reqDic['finished'] = True
+        reqDic['write2file'] = True
+        if len(reqDic['reviceData'])  > 0 and reqDic['write2file']:
+          self.appendToFile(msg.reqId,'HistData',reqDic['contract'].m_symbol,reqDic['contract'].m_secType,reqDic['endDateStr'].split(' ')[0],reqDic['endDateStr'],reqDic['barLength'])
       
     
     
@@ -85,8 +95,7 @@ class HandleHisData:
     return seq1
   
   
-  def getTimeRangeByDay(self,contract,startESTDateStr,endESTDateStr,duration='1 D',barLength='1 min',):
-    pass
+
   
   def getEveryDay(self,contract,duration='1 D',barLength='1 min'):
     currentBeijingDatetime=datetime.datetime.now()
@@ -96,7 +105,8 @@ class HandleHisData:
     
     
     
-  def appendToFile(self,directory,c_symbol,c_secType,endESTDateStr,endESTtimeStr,barLength):
+  def appendToFile(self,tickID,directory,c_symbol,c_secType,endESTDateStr,endESTtimeStr,barLength):
+    
     directory="HistData"
     if not os.path.exists(directory):
       os.makedirs(directory)
@@ -112,11 +122,13 @@ class HandleHisData:
             lastRow = oldRowData[-1]
             lastRowData=lastRow.split(",")
             endtimeStr = ' %s:%s:%s EST' % (lastRowData[3],lastRowData[4],lastRowData[5])
+            #20161211 16:00:00 EST
             if endtimeStr.find('::') :
                 if barLength=='1 day':
                     duration=str((int(endESTDateStr[0:4])-int(lastRow[0:4]))*366+(int(endESTDateStr[4:6])-int(lastRow[5:7]))*31+int(endESTDateStr[6:8])-int(lastRow[8:10]))+' D'
-                    print duration
+                    print "duration:"+duration
                 else:
+                    print lastRow
                     print "barlength too short"
             #barlength is in mins and previous data has time
             elif barLength.find('min')>0:
@@ -129,46 +141,54 @@ class HandleHisData:
         prevRec=0
         oldRowData.append('date_time,Open,High,Low,Close,Volume\n') 
     
-    if self.write2file:
-        Data=self.cleanMerge(oldRowData,self.newRowData)
-        file = open(fileName, 'w')
-        file.writelines(Data)
-        file.close()
-        print len(Data)-prevRec,' of CSV data appended to file: ', fileName
+    if self.tick_contract_Map.has_key(tickID):
+        reqDic = self.tick_contract_Map[tickID]
+        if reqDic['write2file']:
+            
+            Data=self.cleanMerge(oldRowData,reqDic['reviceData'])
+            file = open(fileName, 'w')
+            file.writelines(Data)
+            file.close()
+            print len(Data)-prevRec,' of CSV data appended to file: ', fileName
     
   
-  def reqIbHistData(self,tickId,contract,duration='1 D',barLength='1 min',endDateStr='20161126 23:59:59',whatToShow='TRADES',useRTH=0,formatDate=1):
+  def reqIbHistData(self):
+    while True:
+      if self.taskQueue.empty():
+        break
+      reqDic = self.taskQueue.get()
+      try:
+        print reqDic['contract'],reqDic['duration'],reqDic['barLength']
+        if self.m_conn.isConnected():
+          self.m_conn.reqHistoricalData(reqDic['tickId'],
+                            reqDic['contract'],
+                            reqDic['endDateStr'], # last requested bar date/time
+                            reqDic['duration'],  # quote duration, units: S,D,W,M,Y
+                            reqDic['barLength'],  # bar length
+                            reqDic['whatToShow'],  # what to show
+                            reqDic['useRTH'], reqDic['formatDate'])
+          time.sleep(20)
+        #print "endDateStr="+dateStr + ' 23:59:59 EST' 
+      except Exception,e:
+          print e
     
-    self.m_endESTDateStr=endDateStr[0:8]
-    self.m_endESTtimeStr = ' 23:59:59 EST' #注意前置空格
-    self.m_barLength = barLength
-    self.m_duration=duration
-    if self.m_conn.isConnected():
-      self.m_conn.reqHistoricalData(tickId,
-                          contract,
-                          self.m_endESTDateStr+self.m_endESTtimeStr, # last requested bar date/time
-                          duration,  # quote duration, units: S,D,W,M,Y
-                          barLength,  # bar length
-                          whatToShow,  # what to show
-                          useRTH, formatDate )
-    
-    time.sleep(15)
     return True
+  
+  
      
 
-  def reqSche(self,tickId,contract,duration,endDatetimeStr,barLength,whatToShow='TRADES',useRTH=0,formatDate=1):
+  def reqSche(self,tickId,contract,duration,endDatetimeStr,barLength,whatToShow='TRADES',useRTH=0,formatDate=1,write2file=True,write2redis=False):
     endDatetime = pd.to_datetime(endDatetimeStr)
     durationArr = duration.split(' ')
-    
-    
-    
+
     if barLength == '1 min':
       startDatetime = endDatetime -   int(durationArr[0]) * pd.tseries.offsets.BDay()
       datetimeDelta = endDatetime - startDatetime
-      if barLength == '1 min' and datetimeDelta.days > 1:
+      if barLength == '1 min' and datetimeDelta.days >= 1:
         while startDatetime <= endDatetime:
           if  not self.tick_contract_Map.has_key(tickId):
             self.tick_contract_Map[tickId]={}
+          self.tick_contract_Map[tickId]['tickId'] = tickId
           self.tick_contract_Map[tickId]['contract'] = contract
           self.tick_contract_Map[tickId]['endDateStr']=startDatetime.strftime('%Y%m%d') + " 23:59:59 EST"
           self.tick_contract_Map[tickId]['duration']='2 D'
@@ -176,11 +196,20 @@ class HandleHisData:
           self.tick_contract_Map[tickId]['whatToShow']=whatToShow
           self.tick_contract_Map[tickId]['useRTH']=useRTH
           self.tick_contract_Map[tickId]['formatDate']=formatDate
+          self.tick_contract_Map[tickId]['reviceData']=[]
+          self.tick_contract_Map[tickId]['finished']=False
+          self.tick_contract_Map[tickId]['write2file']=True
+          self.tick_contract_Map[tickId]['write2redis']=False
+          
           startDatetime = startDatetime +  pd.tseries.offsets.BDay()
+          
+          self.taskQueue.put(self.tick_contract_Map[tickId])
+          
           tickId = tickId + 1
     else:
       if  not self.tick_contract_Map.has_key(tickId):
         self.tick_contract_Map[tickId]={}
+      self.tick_contract_Map[tickId]['tickId'] = tickId
       self.tick_contract_Map[tickId]['contract'] = contract
       self.tick_contract_Map[tickId]['endDateStr']=endDatetime.strftime('%Y%m%d') + " 23:59:59 EST"
       self.tick_contract_Map[tickId]['duration'] = duration
@@ -188,6 +217,11 @@ class HandleHisData:
       self.tick_contract_Map[tickId]['whatToShow']=whatToShow
       self.tick_contract_Map[tickId]['useRTH']=useRTH
       self.tick_contract_Map[tickId]['formatDate']=formatDate
+      self.tick_contract_Map[tickId]['reviceData']=[]
+      self.tick_contract_Map[tickId]['finished']=False
+      self.tick_contract_Map[tickId]['write2file']=True
+      self.tick_contract_Map[tickId]['write2redis']=False
+      self.taskQueue.put(self.tick_contract_Map[tickId])
       tickId = tickId + 1  
         
     
@@ -209,35 +243,16 @@ if __name__ == '__main__':
   hhd=HandleHisData(con)
   con.register(hhd.reviceHisData, message.historicalData)
   
-  
-  contract=Util.makeContract(contractSymbol='YM',secType='FUT',expiry='20161216',exchange='ECBOT')
-  
+  #contract=Util.makeContract(contractSymbol='YM',secType='FUT',expiry='20161216',exchange='ECBOT')
+  contract=Util.makeContract(contractSymbol='INDU',secType='IND',exchange='NYSE')
   tickID=1
-  endDateStr=datetime.datetime.now().strftime('%Y%m%d')
-  hhd.reqSche(tickID,contract,'2 D',endDateStr + ' 23:59:59','1 min')
+  hhd.reqSche(tickID,contract,'150 D','20161109' + ' 23:59:59','1 min')
+  
+  
   con.connect()
-  
- 
-  
-  for tickId,reqDic in hhd.tick_contract_Map.items():
-     
-    #对于future，这个区间
-    #  设为2 D请求得到的结果是20161129 18:00:00---20161130 23:59:59，
-    #      1 D得到结果是20161130 18:00:00---20161130 23:59:59
- 
-    try:
-      #print reqDic['contract'],reqDic['duration'],reqDic['barLength']
-      hhd.reqIbHistData(tickId,reqDic['contract'],duration=reqDic['duration'],barLength=reqDic['barLength'],endDateStr= endDateStr + ' 23:59:59 EST')
-      del hhd.newRowData[:]
-      #print "endDateStr="+dateStr + ' 23:59:59 EST'
-      time.sleep(20)
-    except Exception,e:
-      print e
-      con.disconnect()
-      sys.exit()
-         
-  
+  hhd.reqIbHistData()
   #futContact=Util.makeContract(contaceSymbol='ES',secType='FUT',expiry='20161216',exchange='GLOBEX')
   #futContact=Util.makeContract(contaceSymbol='YM',secType='FUT',expiry='20161216',exchange='ECBOT')
   #hhd.getIbHistData(contract=futContact,duration='2 D',barLength='1 min',endDateStr='20161122 23:59:59')
+  #hhd.logFile.close()
   con.disconnect()
