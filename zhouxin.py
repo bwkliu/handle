3afa560f614e3c503c56bd6d0ff935bf5d2ec0ea
bwkliu <bwkliu@qq.com>
2016-12-11 02:05:48 +0800

    commit

--- zhouxin.py
+++ zhouxin.py
@@ -1,7 +1,35 @@
 import pandas as pd
+from Util import Util
+import sys,os,datetime,pytz
 
-
-def read_data():
+def yestodayHLC(yestodayDateStr=''):
+  beijingDatetime = None
+  try:
+    if not yestodayDateStr:
+      beijingDatetime = datetime.datetime.now().strftime('%Y%m%d')
+    else:
+      beijingDatetime = pd.to_datetime(yestodayDateStr)  
+      
+    estDatetime =  Util.coverToEstDatetime(beijingDatetime)
+    startDate= estDatetime  - pd.tseries.offsets.BDay()*2
+    endDate= estDatetime  - pd.tseries.offsets.BDay()*2
+    endFile='g:/ibpy/HistData/%s_%s_%s_1_min.csv' % ('YM','FUT',endDate.strftime('%Y%m%d'))
+    startFile='g:/ibpy/HistData/%s_%s_%s_1_min.csv' % ('YM','FUT',startDate.strftime('%Y%m%d'))
+    startDF=pd.read_csv(startFile)
+    endDF=pd.read_csv(endFile)
+    ymDF=pd.concat([startDF,endDF])
+    ymDF.index=pd.DatetimeIndex(pd.to_datetime(ymDF.date_time))
+    print startDate.strftime('%Y%m%d')+' 16:30' , endDate.strftime('%Y%m%d')+' 16:14'
+    yestodyDF=ymDF[startDate.strftime('%Y%m%d')+' 16:30' : endDate.strftime('%Y%m%d')+' 16:14']
+    print yestodyDF
+    h,l,c=(yestodyDF.High.max(),yestodyDF.Low.min(),yestodyDF.Close[-1])
+    return (h,l,c)
+  except Exception,e:
+    print "yestoday HLC error"
+    print e
+    sys.exit()
+   
+    
   
 
 def zhouxin(h,l,c):
